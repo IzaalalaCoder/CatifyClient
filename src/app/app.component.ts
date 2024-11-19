a7d510d2562f4d7c73ac32931a76b087f7d69294
izana <izana.khabouri@univ-rouen.fr>
2024-11-19 08:01:47 +0100

    feat: add loading and display logic for categories

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,13 +1,31 @@
 import { Component } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
+import { CategoryService } from './services/category.service';
+import { OnInit } from '@angular/core';
+import { CommonModule } from '@angular/common';
 
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [RouterOutlet],
+  imports: [RouterOutlet, CommonModule],
   templateUrl: './app.component.html',
-  styleUrl: './app.component.css'
+  styleUrl: './app.component.css',
+  providers: [CategoryService]
 })
-export class AppComponent {
-  title = 'catify-client';
+export class AppComponent implements OnInit {
+  title = 'Catify';
+
+  categories: any[] = [];
+
+  constructor(private categoryService : CategoryService) {
+
+  }
+
+  ngOnInit(): void {
+    console.log("hehe");
+    this.categoryService.getCategories().subscribe((datas) => {
+      this.categories = datas;
+      console.log(datas);
+    });
+  }
 }
