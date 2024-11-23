55034a4f178c13f7c3d593f1e82a7a72155c0757
izana <izana.khabouri@univ-rouen.fr>
2024-11-24 00:32:36 +0100

    feat: include display list of result search if realize search

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -5,26 +5,44 @@ import { OnInit } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { CategoryListComponent } from './components/category-list/category-list.component';
 import { MenuBarComponent } from './components/menu-bar/menu-bar.component';
+import { CategoryPaginatedListComponent } from './components/category-paginated-list/category-paginated-list.component';
 
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [RouterOutlet, CommonModule, CategoryListComponent, MenuBarComponent],
+  imports: [RouterOutlet, CommonModule, CategoryListComponent, CategoryPaginatedListComponent, MenuBarComponent],
   templateUrl: './app.component.html',
   styleUrl: './app.component.css'
 })
 export class AppComponent implements OnInit {
   title = 'Catify';
 
+  search: boolean = false;
   categories: any[] = [];
+  searchResponse: any = {};
+  searchParams: any = {};
 
-  constructor(private categoryService : CategoryService) {
-
-  }
+  constructor(private categoryService: CategoryService) {}
 
   ngOnInit(): void {
     this.categoryService.getCategories().subscribe((datas) => {
       this.categories = datas;
     });
+  }
+  searchData: any = {
+    response: this.searchResponse,
+    params: this.searchParams
+  };
+
+  gainedResponse(data: any) {
+    this.searchResponse = data.response;
+    this.searchParams = data.params;
+
+    this.searchData = {
+      response: this.searchResponse,
+      params: this.searchParams
+    };
+
+    this.search = true;
   }
 }
